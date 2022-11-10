2835cec9d4305f72fb770bda0cfb9e898470a57a
ArturoEscobar-dev <veloctur@gmail.com>
2022-11-09 22:47:55 -0600

    Lógica de movimientos

---
+++ Proyecto2doParcialMotores/Assets/Script/FighterMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+
+
+    Vector2 movement;
+
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (Input.GetAxisRaw("Horizontal") == 1 ||
+            Input.GetAxisRaw("Horizontal") == -1 ||
+            Input.GetAxisRaw("Vertical") == 1 ||
+            Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("Last_Horizontal",
+                              Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("Last_Vertical",
+                              Input.GetAxisRaw("Vertical"));
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetBool("isAttack", true);
+       
+    }
+
+    
+    private void FixedUpdate()
+    {
+        if (animator.GetBool("isAttack") == true)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    void StopAttack()
+    {
+        if (animator.GetBool("isAttack"))
+            animator.SetBool("isAttack", false);
+    }
+
+
+
+
+}
